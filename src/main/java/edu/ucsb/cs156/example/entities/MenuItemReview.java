51104bf636d6a8812bc5550295d42ea98794d8fb
Onur Yarabras <oyararbas@ucsb.edu>
2025-10-28 20:31:39 -0700

    Added tests to MenuItemReviewController, passed jacoco and pitest coverage.

--- src/main/java/edu/ucsb/cs156/example/entities/MenuItemReview.java
+++ src/main/java/edu/ucsb/cs156/example/entities/MenuItemReview.java
@@ -5,16 +5,22 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity(name = "menuitemreview")
 public class MenuItemReview {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
 
-  private long itemID;
+  private long itemId;
   private String reviewerEmail;
   private int stars;
   private LocalDateTime dateReviewed;
