6d7421ec27c61d2a3773ab9d982c418b0fb24b4b
Samprita Chakraborty <sampritac9@gmail.com>
2025-11-09 13:29:08 -0800

    fixed 500 POST

--- src/main/java/edu/ucsb/cs156/example/controllers/UCSBDiningCommonsMenuItemsController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/UCSBDiningCommonsMenuItemsController.java
@@ -19,9 +19,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-/** This is a REST controller for UCSBDiningCommonsMenuItems */
+// ** This is a REST controller for UCSBDiningCommonsMenuItems */
 @Tag(name = "UCSBDiningCommonsMenuItems")
-@RequestMapping("/api/UCSBDiningCommonsMenuItems")
+@RequestMapping("/api/ucsbdiningcommonsmenuitems")
 @RestController
 @Slf4j
 public class UCSBDiningCommonsMenuItemsController extends ApiController {
